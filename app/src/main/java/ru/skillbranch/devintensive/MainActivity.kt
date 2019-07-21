e393ade147faace01f04165203c36f809ae167d8
Aleksandr Khozyainov <hozalex@gmail.com>
2019-07-21 19:06:05 +0300

    add onEditAction

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -2,19 +2,23 @@ package ru.skillbranch.devintensive
 
 import android.graphics.Color
 import android.graphics.PorterDuff
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, TextView.OnEditorActionListener {
 
-    lateinit var benderImage: ImageView
+    lateinit var benderImage: AppCompatImageView
     lateinit var textTxt: TextView
     lateinit var messageEt: EditText
     lateinit var sendBtn: ImageView
@@ -39,6 +43,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
 
         textTxt.text = (benderObj.askQuestion())
+        messageEt.setOnEditorActionListener(this)
         sendBtn.setOnClickListener(this)
     }
 
@@ -51,6 +56,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             textTxt.text = phrase
         }
     }
+
+    override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            hideKeyboard()
+            val (phrase, color) = benderObj.listenAnswer((messageEt.text.toString()))
+            messageEt.setText("")
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+            return true
+        }
+        return false
+    }
+
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -7,7 +7,7 @@ import android.view.inputmethod.InputMethodManager
 /**
  * Created by A.Khozyainov on 17.07.19
  */
-fun Activity.hideKeyboard(context: Context) {
-    var im: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+fun Activity.hideKeyboard() {
+    val im: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     im.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
 }
