00e194bf6fc6e4b125d7b80718481a9a847fcf3f
hozalex <hozalex@gmail.com>
2019-09-01 22:27:28 +0300

    home work

--- app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package ru.skillbranch.devintensive.ui.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViewModel() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
     private fun initViews() {
