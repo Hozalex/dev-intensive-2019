2494d18a2f75901c2d90d2a3ccd51bd4da4a063b
hozalex <hozalex@gmail.com>
2019-09-03 22:56:43 +0300

    home work

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AvatarImageView.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.view.View
+
+class AvatarImageView(context: Context?) : View(context) {
+    fun setInitials(initials: String) {
+
+    }
+}
